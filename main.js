000a223f7cce7941b296295c36305face55dd1b2
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-01-28 20:27:51 -0300

    primeira funcao em js criada

--- main.js
+++ main.js
@@ -4,3 +4,9 @@
 //Responsive menu
 //Botão 1 - Dark e light mode
 //Botão 2 - Alert
+
+const alerta = () => {
+    window.alert('Alerta!')
+}
+
+document.querySelector(".button-outline").addEventListener('click', alerta)
