478632b24f2cd252a4b8f290600ee0e330592fb3
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-02-01 02:20:18 -0300

    adicao de comentarios

--- main.js
+++ main.js
@@ -4,9 +4,24 @@
 //Responsive menu
 //Botão 1 - Dark e light mode
 //Botão 2 - Alert
+//Seção com 3 icones com animação de 'aparecer'
+//Carrossel
+
+//Próximo passo - Criar o menu com o javascript
 
 const alerta = () => {
     window.alert('Alerta!')
 }
 
 document.querySelector(".button-outline").addEventListener('click', alerta)
+
+/*Darkmode - Switch
+- Trocar o background
+- Cor da fonte
+- Cor do background de todo o site, inclusive no menu
+- Cor dos botões [talvez]
+- Trocar o texto do botão para Light mode
+*/
+
+//Caso tenha mais conteúdo na página, adiciona a personalização do scroll
+//Adicionar CSS do darkmode e implementar o código JS com classToggle
